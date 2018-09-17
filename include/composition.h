a5507cc5a8d1dd89d930af268a5f5a3eded8abb7
Ti <872591581@qq.com>
2018-09-17 23:52:32 +0800

    new changes of composition:set up a structure for the robot

---
+++ include/composition.h
@@ -0,0 +1,8 @@
+#ifndef zuhe.h
+#define zuhe.h
+extern void backbodyhead(ROBOT_CASE robot_position);
+extern void left(ROBOT_CASE robot_position);
+extern void forebodyhead(ROBOT_CASE robot_position);
+extern void leftside(ROBOT_CASE robot_position);
+extern void right(ROBOT_CASE robot_position);
+#endif
